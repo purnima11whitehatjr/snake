1045fdec207074319b7dea849923a88ad185ef9e
purnima11whitehatjr <72076800+purnima11whitehatjr@users.noreply.github.com>
2021-01-24 13:54:13 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# snake
+# Snake Game
+Snake game with p5.js.
+
+(C31 Project)
+
+## [Click Here](https://dipamsen.github.io/C31-Snake-Game/) to Play Now!
---
+++ sketch.js
@@ -0,0 +1,105 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg;
+var constrainedLog; 
+var length = 1; 
+var grid = 20;
+
+var cols = 20;
+var rows = 20;
+var gameState = "Play";
+var foodPos;
+var snake;
+var score = 0;
+var snakearr = [];
+function preload() {
+    backgroundImg = loadImage("snakebackground.jpg");
+}
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    
+    frameRate(5);
+    world = engine.world;
+    snake = new Snake(1,1);
+    pickLocation();
+}
+function pickLocation(){
+  foodPos = {
+    x:20*(round(random(0,19))),
+    y:20*(round(random(0,19))),
+  };
+}
+function draw(){
+    background(backgroundImg);
+
+
+    Engine.update(engine);
+   // console.log(snake.x);
+   // console.log(snake.y);
+   // console.log(dist(foodPos.x,foodPos.y,snake.x,snake.y));
+    //console.log(dist(snake.x,snake.y,snake.snake[i].x/2,snake.snake[i].y/2)) 
+    console.log(key);
+    fill(255,0,100);
+   
+    if(snake.eat(foodPos.x,foodPos.y)){
+      pickLocation();
+    }
+    fill("lightblue");
+  textSize(20);
+  textAlign(CENTER,CENTER);
+  textFont("Ebrima");
+  textStyle(BOLD);
+  if(gameState === "Play"){
+  text("Score: " + score, 350,20);
+  }
+  
+    console.log(gameState);
+    
+    if(gameState === "Over"){
+        fill("white");
+        textSize(120);
+        textAlign(CENTER);
+        snake.dir(0,0);
+        textFont("Arial");
+        textStyle(NORMAL);      
+        text(score,200,200);
+        textSize(18);
+        text("Refresh this page to Play Again")
+    }
+    fill(255,0,100);
+    stroke(50);
+    rect(foodPos.x, foodPos.y,grid,grid);
+    if(gameState === "Play"){
+      snake.over();
+      snake.move();
+      snake.display();
+    }
+
+    
+
+
+}
+
+
+function keyPressed(){
+    if(keyCode === UP_ARROW && gameState==="Play"){
+      snake.dir(0,-1);
+    }
+    if(keyCode === DOWN_ARROW && gameState==="Play"){
+      snake.dir(0,1);
+    }
+    if(keyCode === RIGHT_ARROW && gameState==="Play"){
+      snake.dir(1,0);
+    }
+    if(keyCode === LEFT_ARROW && gameState==="Play"){
+      snake.dir(-1,0);
+    }
+    
+  }
